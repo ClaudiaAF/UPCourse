ee1382af6751ab3262f5e47c8c98d7b584ab3c70
claud <180181@virtualwindow.co.za>
2021-08-26 12:46:44 +0200

    Add tests

---
+++ src/main/kotlin/tests/FundsPoolTest.kt
@@ -0,0 +1,153 @@
+package tests
+
+import com.example.demo.view.FundsPool
+
+internal class FundsPoolTest {
+
+    @org.junit.jupiter.api.Test
+    fun getOverallExpense() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.overallExpense
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setOverallExpense() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.overallExpense
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getLecturerSalariesLabel() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.lecturerSalariesLabel
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setLecturerSalariesLabel() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.lecturerSalariesLabel
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getLecturerSalariesProperty() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.lecturerSalariesProperty
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getSubjectsPriceLabel() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.subjectsPriceLabel
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setSubjectsPriceLabel() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.subjectsPriceLabel
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getSubjectsPriceProperty() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.subjectsPriceProperty
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getDiplomaFeesLabel() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.diplomaFeesLabel
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setDiplomaFeesLabel() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.diplomaFeesLabel
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getDiplomaFeesProperty() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.diplomaFeesProperty
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getDegreeFeesLabel() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.degreeFeesLabel
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setDegreeFeesLabel() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.degreeFeesLabel
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getDegreeFeesProperty() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.degreeFeesProperty
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getAdminSalariesLabel() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.adminSalariesLabel
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setAdminSalariesLabel() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.adminSalariesLabel
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getAdminSalariesProperty() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.adminSalariesProperty
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getTotalCostNumber() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.totalCostNumber
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setTotalCostNumber() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.totalCostNumber
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getPieItemsData() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.pieItemsData
+        assert(value = true)
+    }
+
+    @org.junit.jupiter.api.Test
+    fun setPieItemsData() {
+        val fundsPool = FundsPool()
+        val result = fundsPool.pieItemsData
+        assert(value = true)
+    }
+}
